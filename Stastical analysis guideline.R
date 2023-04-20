b4227415112b5204f48661a988196a2b46813169
kiyunlee <lgybrian@gmail.com>
2023-04-19 19:38:02 -0500

    Modify to be suitable for the example dataset

--- Stastical analysis guideline.R
+++ Stastical analysis guideline.R
@@ -1,34 +1,38 @@
 # created: 8/15/2021. By kiyunlee
-# modified: 4/11/2023/ By kiyunlee
+# modified: 4/19/2023/ By kiyunlee
 
 # note
 # This program is a guideline for stastical analysis.
-# The program is useful to perform entry level of stastical analysis from the beginning.
-# The program contains statistical models, normal distribution tests, data transformation, hypothesis tests, post-hoc testsl.
+# The program is suitable for biotech/neuroscience/pharamaceutical field where my PhD study and industry experience were in.
+# The program contains statistical models, normal distribution tests, data transformation, hypothesis tests, post-hoc tests.
+# The program is designed to run by selection, not the whole.
+# Please refer to the example dataset "stastical analysis example.txt". This program is modified and adapted to the dataset.
 
 
 
+
+# ///
 # handle data
 # import data
-data <- read.delim("result.txt",header = TRUE)
+data <- read.delim("stastical analysis example.txt",header = TRUE)
 
 # remove NaN values
 data <- na.omit(data)
 
 # create subsets
-sub <- subset(data, feature1!='15min')
-sub <- subset(data, feature2=='100nM' | feature2=='150nM')
+sub <- subset(data, drug='d1')
+# ///
 
 
 
 
 # build a linear regression model
-model <- lm(target1 ~ feature1*feature2*feature3, data)
+model <- lm(numSynapse ~ drug*time, data)
 
 
 
 
-
+# ///
 # perform various normal distribution tests
 install.packages("ggpubr")
 library(ggpubr)
@@ -48,65 +52,57 @@ install.packages("moments")
 library(moments)
 skewness(model$resid)
 kurtosis(model$resid)
+# ///
 
 
 
 
-
-# if not normally distributed, power transform (Box-Cox)
+# ///
+# if not normally distributed
+# option 1: power transform (Box-Cox)
 library(MASS)
 bc = boxcox(model, lambda = seq(-3,3))
 best.lam = bc$x[which(bc$y == max(bc$y))]
-data$target1Trans <- data$target1^best.lam
-model <- lm(target1Trans ~ feature1*feature2*feature3, data)
+data$numSynapseTrans <- data$numSynapse^best.lam
+model <- lm(numSynapseTrans ~ drug*time, data)
 
 
-
-
-
-# if not normally distributed, remove a few data points
-# order data by residual
+# option 2: remove a few data points
 df <- data
 df$resid <- model$resid
-df <- df[order(df$resid),]
+df <- df[order(df$resid),] # order data by residual
 head(df)
 tail(df)
 
 
-
-
-#if not normally distributed, Non-normal distribution
-# Kruskal-Wallis test.  To compare three or more groups
+# option 3: non-normal distribution
 install.packages('tidyverse')
 library(tidyverse)
 
-data$feature1 <- as.factor(data$feature1)
-data$feature2 <- as.factor(data$feature2)
+data$drug <- as.factor(data$drug)
+data$time <- as.factor(data$time)
+data$drug_time <- with(data, interaction(data, time)) # interaction
 
-data$feature1_2 <- with(data, interaction(feature1, feature2)) # interaction
-kw <- kruskal.test(model, data=data)
-
-
+kw <- kruskal.test(model, data=data) # Kruskal-Wallis test.  To compare three or more groups
 
 
-#if not normally distributed, Non-normal distribution
-#Aligned Ranks Transformation ANOVA for non-parametric test
+# option 4: non-normal distribution
 install.packages("ARTool")
 library(ARTool)
 
-data$feature1_2 <- with(data, interaction(feature1, feature2)) # interaction
+data$drug <- as.factor(data$drug)
+data$time <- as.factor(data$time)
+data$drug_time <- with(data, interaction(data, time)) # interaction
 
-data$feature1 <- as.factor(data$feature1)
-data$feature2 <- as.factor(data$feature2)
-
-model <- art(target1 ~ feature1*feature2*feature3, data)
+model <- art(target1 ~ feature1*feature2*feature3, data) # Aligned Ranks Transformation ANOVA for non-parametric test
 anova(model)
+# ///
 
 
 
 
 
-
+# ///
 # perform hypothesis tests
 
 # variance test
@@ -122,11 +118,8 @@ help(t-test)
 # ANOVA
 anova(model)
 aov_model <- aov(model)
+# ///
 
-
-
-#marginal = lsmeans(lme.repeated, ~ treat*drug*ages)
-cld(marginal, alpha   = 0.05, Letters = letters,  adjust  = "tukey")
 
 
 
@@ -147,23 +140,20 @@ tukey <- HSD.test(aov_fm,"ages:treat")
 
 
 
-
 # Fisher's Least Significant Different Test
 # option 1
 library("DescTools")
 fisher <- PostHocTest(aov_model, method = "lsd")
 
 # option 2
-fisher <- LSD.test(aov_model, "ages", p.adj="bonferroni")
-
-
-
+fisher <- LSD.test(aov_model, "drug", p.adj="bonferroni")
 
 
 
 # post hoc for Aligned Ranks Transformation
-marginal = art.con(model, "feature1:feature2", adjust="none") #LSD
-marginal = art.con(model, "feature1:feature2", adjust="tukey") #tukey
+marginal = art.con(model, "drug:time", adjust="tukey") # tukey
+marginal = art.con(model, "drug:time", adjust="none") # LSD
+# ///
 
 
 
@@ -172,7 +162,7 @@ marginal = art.con(model, "feature1:feature2", adjust="tukey") #tukey
 # add significance letters
 install.packages('emmeans')
 library("emmeans")
-em <- emmeans(aov_model,~feature1*feature2*feature3)
+em <- emmeans(aov_model,~drug*time)
 
 summary(pairs(em, adjust="none"), infer=TRUE)
 
